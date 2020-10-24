ab3cdc5aca094f26d4701bcb492604805b4f4e8d
samliu000 <44206874+samliu000@users.noreply.github.com>
2020-10-24 06:56:13 -0700

    Updated

--- forKimberly.java
+++ forKimberly.java
@@ -5,3 +5,35 @@ public class Main {
 	}
 }
 	
+/*
+I'm pretending to type stuff so kimberly thinks that her gift
+is mainly text based
+but little does she know
+that it is actually something other than that haha
+haha
+haaha
+hahahahaha
+HAHAHA
+MUAHAHA
+something soething
+soething else is her e s well
+bloop blop beep beep
+bop bop
+poo
+pepe pee poo poo
+this item here is blah blah lbha it is a memory of the first time we blank
+and here is something that is ideal haha
+hoohoo
+but like in the end it is poee pee poo poo
+Script time:
+Friend: Goes to check home of his friend
+Home: Empty
+Friend: oh god, they must've gotten deported
+Home: Movement
+Friend: turns around
+Coochieman: 
+Friend: Omg how did you not get deported
+Coochieman: i'm different
+i'm pretending totype agian to divert attention
+this is rly dangerous because she could find out at any tmoment
+;lkj;
